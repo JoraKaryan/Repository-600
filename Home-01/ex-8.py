3ff0c331060ea3078ab31cf3c972fe4d1457bd16
JoraKaryan <63347929+JoraKaryan@users.noreply.github.com>
2020-04-14 02:12:06 +0400

    Add files via upload

--- Home-01/ex-8.py
+++ Home-01/ex-8.py
@@ -1,12 +1,14 @@
 #ex-8
 
-mylist = [1,2,3,5,6,7,8]
+mylist = [4,2,1]
+
+
 def foo(l):
-    for i in l:
-        if l[i+1] != l[i]+1:
-            return l[i]+1
-        if l[i+1] == l[i]+1:
-            return l[-1]+1
+    for i in range(1,len(l)+1):
+        if len(l) == max(l):
+            return max(l)+1
+        if i not in l:
+            return i
     if len(l) == 0:
         return 1
 print(foo(mylist))
