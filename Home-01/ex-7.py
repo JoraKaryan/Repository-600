169b0bfae72fca4de7144cee174787a51959755b
JoraKaryan <63347929+JoraKaryan@users.noreply.github.com>
2020-04-14 04:10:30 +0400

    Add files via upload

--- Home-01/ex-7.py
+++ Home-01/ex-7.py
@@ -1,12 +1,20 @@
 #ex-7
 
-bar = "What is this book about? this book is about python coding"
+bar = "What! is! this book about !this book! is about python coding"
 
-if "?" in bar:
-    bar = bar.replace('?', "")
+def foo(bar:str):
+    for i in range(len(bar)):
+        if 0 <= ord(bar[i]) <= 31 or 33<= ord(bar[i])<=64 or 91 <= ord(bar[i]) <= 96 or 123 <= ord(bar[i]) <= 127:
+            bar= bar.replace(bar[i], "")
+            return bar
+
+bar = (foo(bar))
+
 l = bar.split(" ")
 
 c = []
 for i in range(len(l)):
     c.append(l.count(l[i]))
 print(dict(zip(l, c)))
+
+
