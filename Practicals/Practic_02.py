d7b84df726e9efab6b2b24027f9563ae6c10cc3c
JoraKaryan <63347929+JoraKaryan@users.noreply.github.com>
2020-04-16 20:55:22 +0400

    Add files via upload

---
+++ Practicals/Practic_02.py
@@ -0,0 +1,22 @@
+#3
+
+l = [False, False, True]
+
+def quick_or(l):
+    x=[False for i in range(len(l))]
+    return x==l
+
+print(quick_or(l))
+
+
+#5
+
+def last_digit(n):
+    s=[x**2 for x in range(1,n+1)]
+    d=0
+    for i in s:
+        d = d+i**2
+    return d%10
+print(last_digit(950))
+
+
